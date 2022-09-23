e633a39e1ef2e1473167dac3e88283d1541e3d66
刘大大 <893703953@qq.com>
2022-09-23 19:24:54 +0800

    Update go.go

--- Operators/go.go
+++ Operators/go.go
@@ -22,4 +22,33 @@ func main() {
 	a = 21 // 为了方便测试，a 这里重新赋值为 21
 	a--
 	fmt.Printf("第七行 - a 的值为 %d\n", a)
+
+	var d int = 21
+	var e int = 10
+
+	if d == e {
+		fmt.Printf("第一行 - d 等于 e\n")
+	} else {
+		fmt.Printf("第一行 - d 不等于 e\n")
+	}
+	if d < e {
+		fmt.Printf("第二行 - d 小于 e\n")
+	} else {
+		fmt.Printf("第二行 - d 不小于 e\n")
+	}
+
+	if d > e {
+		fmt.Printf("第三行 - d 大于 e\n")
+	} else {
+		fmt.Printf("第三行 - d 不大于 e\n")
+	}
+
+	d = 5
+	e = 20
+	if d <= e {
+		fmt.Printf("第四行 - d 小于等于 e\n")
+	}
+	if e >= d {
+		fmt.Printf("第五行 - e 大于等于 d\n")
+	}
 }
