8fb4cc57c750e3ee7b9efacad0d47b66f8f5c8f6
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-09-26 22:18:27 +0800

    Update go.go

--- Functions/go.go
+++ Functions/go.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	/* 定义局部变量 */
@@ -12,6 +15,29 @@ func main() {
 	ret = max(a, b)
 
 	fmt.Printf("最大值是 : %d\n", ret)
+
+	hello("刘大大")
+
+	x, y := swap("Google", "Runoob")
+	fmt.Println(x, y)
+
+	/* 声明函数变量 */
+	getSquareRoot := func(x float64) float64 {
+		return math.Sqrt(x)
+	}
+
+	/* 使用函数 */
+	fmt.Println(getSquareRoot(9))
+
+	testCallBack(1, callBack)
+	testCallBack(2, func(x int) int {
+		fmt.Printf("我是回调，x：%d\n", x)
+		return x
+	})
+
+	var c1 Circle
+	c1.radius = 10.00
+	fmt.Println("圆的面积 = ", c1.getArea())
 }
 
 /* 函数返回两个数的最大值 */
@@ -25,4 +51,37 @@ func max(num1, num2 int) int {
 		result = num2
 	}
 	return result
+}
+
+/* 哈喽 */
+func hello(name string) {
+	fmt.Printf("hello " + name)
+}
+
+/* 包裹 */
+func swap(x string, y string) (string, string) {
+	return x + " a", y + " b"
+}
+
+// 声明一个函数类型
+type cb func(int) int
+
+func testCallBack(x int, f cb) {
+	f(x)
+}
+
+func callBack(x int) int {
+	fmt.Printf("我是回调，x：%d\n", x)
+	return x
+}
+
+/* 定义结构体 */
+type Circle struct {
+	radius float64
+}
+
+// 该 method 属于 Circle 类型对象中的方法
+func (c Circle) getArea() float64 {
+	//c.radius 即为 Circle 类型对象中的属性
+	return 3.14 * c.radius * c.radius
 }
