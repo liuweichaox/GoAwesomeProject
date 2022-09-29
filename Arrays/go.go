c547447a014f37976d18ecbf749e58c4c3abbf2b
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-09-30 00:02:17 +0800

    Update go.go

--- Arrays/go.go
+++ Arrays/go.go
@@ -23,4 +23,23 @@ func main() {
 	for k = 0; k < 5; k++ {
 		fmt.Printf("balance3[%d] = %f\n", k, balance3[k])
 	}
+
+	// Step 1: 创建数组
+	values := [][]int{}
+
+	// Step 2: 使用 append() 函数向空的二维数组添加两行一维数组
+	row1 := []int{1, 2, 3}
+	row2 := []int{4, 5, 6}
+	values = append(values, row1)
+	values = append(values, row2)
+
+	// Step 3: 显示两行数据
+	fmt.Println("Row 1")
+	fmt.Println(values[0])
+	fmt.Println("Row 2")
+	fmt.Println(values[1])
+
+	// Step 4: 访问第一个元素
+	fmt.Println("第一个元素为：")
+	fmt.Println(values[0][0])
 }
