7cbdfa8990dfe63fbdf1fb6623c1cd6142549ed8
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-10-03 22:10:26 +0800

    Update go.go

--- Interfaces/go.go
+++ Interfaces/go.go
@@ -22,13 +22,6 @@ func (applePhone ApplePhone) call() {
 	fmt.Println("I am Apple, I can call you!")
 }
 
-type XiaoMiPhone struct {
-}
-
-func (xiaoMiPhone XiaoMiPhone) call() {
-	fmt.Println("I am XiaoMi, I can call you!")
-}
-
 func main() {
 	var phone Phone
 
@@ -36,8 +29,5 @@ func main() {
 	phone.call()
 
 	phone = new(ApplePhone)
-	phone.call()
-
-	phone = new(XiaoMiPhone)
 	phone.call()
 }
