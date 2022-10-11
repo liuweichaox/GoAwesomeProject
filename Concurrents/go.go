57be8185f4224c53e152e51cb1e957a9afb6d495
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-10-11 23:43:21 +0800

    go

--- Concurrents/go.go
+++ Concurrents/go.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -13,10 +14,25 @@ func say(s string, c chan string) {
 	close(c)
 }
 
+var wg sync.WaitGroup
+
+func hello(i int) {
+	defer wg.Done() // goroutine结束就登记-1
+	fmt.Println("Hello Goroutine!", i)
+}
+
 func main() {
 	fmt.Println("start ", time.Now())
 	c := make(chan string)
 	go say("hello", c)
 	result := <-c
 	fmt.Println("end ", result, " ", time.Now())
+
+	//启动多个goroutine
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // 启动一个goroutine就登记+1
+		go hello(i)
+	}
+	wg.Wait() // 等待所有登记的goroutine都结束
+	
 }
