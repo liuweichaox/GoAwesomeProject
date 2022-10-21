f89703052542b32fb6e2c548b61c07e4388f80ef
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-10-21 22:27:06 +0800

    Update go.go

--- Base64Encoding/go.go
+++ Base64Encoding/go.go
@@ -1,11 +1,9 @@
-// Go provides built-in support for [base64
-// encoding/decoding](https://en.wikipedia.org/wiki/Base64).
+// Go 提供了对 [base64 编解码](http://zh.wikipedia.org/wiki/Base64)的内建支持。
 
 package main
 
-// This syntax imports the `encoding/base64` package with
-// the `b64` name instead of the default `base64`. It'll
-// save us some space below.
+// 这个语法引入了 `encoding/base64` 包，
+// 并使用别名 `b64` 代替默认的 `base64`。这样可以节省点空间。
 import (
 	b64 "encoding/base64"
 	"fmt"
@@ -13,25 +11,22 @@ import (
 
 func main() {
 
-	// Here's the `string` we'll encode/decode.
+	// 这是要编解码的字符串。
 	data := "abc123!?$*&()'-=@~"
 
-	// Go supports both standard and URL-compatible
-	// base64. Here's how to encode using the standard
-	// encoder. The encoder requires a `[]byte` so we
-	// convert our `string` to that type.
+	// Go 同时支持标准 base64 以及 URL 兼容 base64。
+	// 这是使用标准编码器进行编码的方法。
+	// 编码器需要一个 `[]byte`，因此我们将 string 转换为该类型。
 	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
 	fmt.Println(sEnc)
 
-	// Decoding may return an error, which you can check
-	// if you don't already know the input to be
-	// well-formed.
+	// 解码可能会返回错误，如果不确定输入信息格式是否正确，
+	// 那么，你就需要进行错误检查了。
 	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
 	fmt.Println(string(sDec))
 	fmt.Println()
 
-	// This encodes/decodes using a URL-compatible base64
-	// format.
+	// 使用 URL base64 格式进行编解码。
 	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
 	fmt.Println(uEnc)
 	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
